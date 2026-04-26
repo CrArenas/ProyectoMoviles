eb59021026500c93a195bdfefcf332f8f115275d
Cristian Andres Arenas Vargas <crarenasv@unal.edu.co>
2026-04-26 17:02:03 -0500

    Rutas implementadas a todos los controladores

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -3,5 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\Room_typeController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CompanionController;
 
-Route::apiResource('/roles', RoleController::class);
+Route::apiResource('roles', RoleController::class);
+Route::apiResource('room_types', Room_typeController::class);
+Route::apiResource('rooms', RoomController::class);
+Route::apiResource('payments', PaymentController::class);
+Route::apiResource('reservations', ReservationController::class);
+Route::apiResource('users', UserController::class);
+Route::apiResource('companions', CompanionController::class);
